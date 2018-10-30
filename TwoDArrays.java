9ece21d02f38c28d2dc2de99cffe658090d21d91
Michael Romero <michael@Michaels-MBP-2.hsd1.ca.comcast.net>
2018-10-29 22:00:19 -0700

    first commit

---
+++ TwoDArrays.java
@@ -0,0 +1,85 @@
+package Integer;
+
+public class TwoDArrays {
+	
+	private int [][] arrayInts;
+	
+
+    /**
+     * Constructor for objects of class Util2DInteger
+     * @param array to array
+     */
+	
+    public TwoDArrays(int [][] arrayInts)
+    {
+        this.arrayInts = arrayInts;
+    }
+   
+    /**
+     * Gets the largest integer in the array
+     * @return the largest number in the array
+     */
+    public int getLargest()
+    {
+        int largest = arrayInts[0][0];//smallest int possible
+        for(int a = 0; a < arrayInts.length; a++)
+        {
+            for(int b = 0; b < arrayInts.length; b++)
+            {
+                if(arrayInts[a][b] > largest)
+                {
+                    largest = arrayInts[a][b];//replace
+                }
+            }
+        }
+        return largest;
+    }
+
+    /**
+     * Gets the product of the elements in the array
+     * @return the product of the array
+     */
+    public int product()
+    {
+        int product = 1;//hold value, 1 wont make a change
+        for(int x = 0; x < arrayInts.length; x++)
+        {
+            for(int y = 0; y < arrayInts[0].length; y++)
+            {
+                product = product * arrayInts[x][y];
+            }
+        }
+        return product;
+    }
+
+    /**
+     * Gets the element in the last column of the last row
+     * @return the element in the last column of the last row
+     */
+    public int lastElement()
+    {
+    	//row,column
+        return arrayInts[arrayInts.length -1][arrayInts[0].length -1];
+    }
+
+    /**
+     * Returns true if the given number is in the array
+     * @param number to array
+     * @return true if the number is inside the array 
+     */
+    public boolean contains(int number)
+    {
+        boolean check = false;
+        for(int i = 0; i < arrayInts.length; i++)//rows
+        {
+            for(int z = 0; z < arrayInts[0].length; z++)//columns
+            {
+                if(arrayInts[i][z] == number)
+                {
+                    check = true;
+                }
+            }
+        }
+        return check;
+    }
+}
